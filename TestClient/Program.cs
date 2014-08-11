42b200ee7bb01478595d1b28b70b45d5d642c3f4
Fernando Oliva <fernando.oliva.hueto@gmail.com>
2014-08-11 15:36:29 +0200

    First Commit
    Security Tools

---
+++ TestClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string plainText = "test";
+
+            Console.WriteLine(SecurityTools.Encrypting.Encrypt(plainText));
+            Console.ReadLine();
+            Console.WriteLine(SecurityTools.Encrypting.Decrypt(SecurityTools.Encrypting.Encrypt(plainText)));
+            Console.ReadLine();
+        }
+    }
+}
